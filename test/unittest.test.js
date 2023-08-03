8e133a20e6e1497f16024d880722f6f4a9e0ef48
Piyush Kumar <piyush.kumar@ingenxtec.com>
2023-08-03 05:00:15 +0000

    removed the commnet

--- test/unittest.test.js
+++ test/unittest.test.js
@@ -2,7 +2,6 @@ const cds = require("@sap/cds");
 const { expect, GET, POST } = cds.test.in(__dirname, "..").run(
     "serve", "--with-mocks", "--in-memory");
     
-  // New changes to push the code to github  -2
 describe("Testing OData APIs", () => {
   it("test status codes", async () => {
     const { data } = await GET`/ems/Statuses?$select=code`;
